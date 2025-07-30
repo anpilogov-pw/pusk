ea3197db500c485d516ea39d806134cadbf9be8b
Artyom Anpilogov <47990465+fluffytrolll@users.noreply.github.com>
2025-07-30 11:08:06 +0300

    Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
-    <h1>{{ $t('home') }}</h1>
-    <router-link to="/about">
-      {{ $t('about') }}
-    </router-link>
+  <div class="text-center">
+    <div class="mb-8">
+      <h1 class="text-5xl font-bold text-gray-800 mb-4">
+        {{ $t('home') }}
+      </h1>
+      <p class="text-xl text-gray-600 mb-8">
+        Добро пожаловать в наше приложение
+      </p>
+    </div>
+    
+    <div class="space-y-4">
+      <router-link 
+        to="/about" 
+        class="inline-block px-8 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200 shadow-lg"
+      >
+        {{ $t('about') }}
+      </router-link>
+    </div>
   </div>
 </template>
